1ce3e6baebd799bd25dcdbe8babce7cc39ef480e
KUNWOO KIM <gundam1110@naver.com>
2021-05-16 10:29:01 +0900

    Create 01_time_check.py

---
+++ python_study/coding/01_time_check.py
@@ -0,0 +1,9 @@
+import time
+start_time = time.time()    # 측정 시작
+
+# 프로그램 소스코드
+end_time = time.time()
+
+print("start time : ", start_time) # 수행 시간 출력
+print("end time : ", end_time) # 수행 시간 출력
+print("time : ", end_time - start_time) # 수행 시간 출력
